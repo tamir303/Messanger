5ddc436f03e597b2a10168d098bf3688435acdd2
Tamir <tamir303@gmail.com>
2023-07-15 18:20:49 +0300

    create conversation model when press on user

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Avatar from "@/app/components/Avatar";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -42,8 +43,27 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
                 transition
                 cursor-pointer"
     >
-      <Avatar />
-      
+      <Avatar user={data} />
+      <div className="main-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div
+            className="
+                flex
+                justify-between
+                items-center
+                mb-1"
+          >
+            <p
+              className="
+                      text-sm
+                      font-medium
+                      text-gray-900"
+            >
+              {data.name}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
